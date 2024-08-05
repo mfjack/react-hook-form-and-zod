f1698f9346bddf0e6623a4f26fde81ef7825845b
Marlon Ferreira <ferreira.marlon@live.com>
2024-08-05 15:59:15 -0300

    feat: Added the function to show and hide the password

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import Button from "./_components/Button";
 import Input from "./_components/Input";
+import { IoEyeSharp } from "react-icons/io5";
+import { HiMiniEyeSlash } from "react-icons/hi2";
 
 const Form = () => {
+   const [passwordVisible, setPasswordVisible] = useState(false);
+
+   const handlePasswordPassword = () => {
+      setPasswordVisible(!passwordVisible);
+   };
+
    return (
       <form className="flex h-screen flex-col items-center justify-center gap-4 p-5">
          <Input type="text" placeholder="Nome Completo" />
+
          <Input type="email" placeholder="E-mail" />
-         <Input type="password" placeholder="Senha" />
-         <Input type="password" placeholder="Confirmar Senha" />
+
+         <div className="relative w-full">
+            <Input type={passwordVisible ? "text" : "password"} placeholder="Senha" />
+            <button className="absolute right-3 top-3" type="button" onClick={handlePasswordPassword}>
+               {passwordVisible ? <HiMiniEyeSlash size={20} /> : <IoEyeSharp size={20} />}
+            </button>
+         </div>
+
+         <div className="relative w-full">
+            <Input type={passwordVisible ? "text" : "password"} placeholder="Confirmar Senha" />
+            <button className="absolute right-3 top-3" type="button" onClick={handlePasswordPassword}>
+               {passwordVisible ? <HiMiniEyeSlash size={20} /> : <IoEyeSharp size={20} />}
+            </button>
+         </div>
+         
          <Input type="text" placeholder="Celular" />
          <Input type="text" placeholder="CPF" />
          <Input type="text" placeholder="CEP" />
